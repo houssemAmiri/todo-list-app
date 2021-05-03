9fd06df8899e874d97bd4ecda6161b0f2fa28b18
Amiri Houssem <houssem.amiri2@gmail.com>
2021-05-03 22:09:08 +0100

    Todo list list todos from store redux

--- src/components/Tasks/TaskList.tsx
+++ src/components/Tasks/TaskList.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 
 import { ListTasksText } from "../../constants/text";
-
-const TaskList: React.FC = () => {
+interface Props {
+  todoList: ITodo[];
+}
+const TaskList: React.FC<Props> = ({ todoList }) => {
   return (
     <>
       <h2>{ListTasksText.TITLE}</h2>
       <ul className="list-group">
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-          Cras justo odio
-          <span className="badge badge-primary badge-pill">14</span>
-        </li>
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-          Dapibus ac facilisis in
-          <span className="badge badge-primary badge-pill">2</span>
-        </li>
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-          Morbi leo risus
-          <span className="badge badge-primary badge-pill">1</span>
-        </li>
+        {todoList.map((todo) => (
+          <li className="list-group-item d-flex justify-content-between align-items-center">
+            <h6>
+              <b>{todo.name}:</b> {todo.description}
+            </h6>
+            <span
+              className={`badge ${
+                todo.isCompleted ? "badge-success" : "badge-danger"
+              } badge-pill`}
+            >
+              {todo.isCompleted ? `Complète` : `Non Complete`}
+            </span>
+          </li>
+        ))}
       </ul>
     </>
   );
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useSelector, shallowEqual, useDispatch } from "react-redux";
+import { Dispatch } from "redux";
+
 import AddTask from "../components/Tasks/AddTask";
 import TaskList from "../components/Tasks/TaskList";
+import { addTodo } from "../store/actions/todoActions";
 
 const Tasks: React.FC = () => {
+  const todos: ITodo[] = useSelector(
+    (state: TodoState) => state.todos,
+    shallowEqual
+  );
+  const dispatch: Dispatch<any> = useDispatch();
+  const saveTodo = React.useCallback((todo: ITodo) => dispatch(addTodo(todo)), [
+    dispatch,
+  ]);
+  console.log(todos);
   return (
     <div className="container">
-      <TaskList />
+      <TaskList todoList={todos} />
       <AddTask />
     </div>
   );
